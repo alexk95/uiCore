33e69dbf1398b859037d677f951ca68c401f4f02
Alexander Küster <alexkuester@mail.ru>
2021-06-12 16:06:29 +0200

    Added more functions to the aDateHelper

--- include/akCore/aDateHelper.h
+++ include/akCore/aDateHelper.h
@@ -6,8 +6,17 @@
 namespace ak {
 
 	class UICORE_API_EXPORT aDateHelper {
+
 	public:
 		static QTime addSeconds(const QTime & _orig, int _sec, bool & _overflow);
+
+		static QTime addTime(const QTime & _orig, int _h, int _m, int _s, bool & _overflow);
+
+		static QTime addTime(const QTime & _orig, int _h, int _m, int _s, int _ms, bool & _overflow);
+
+		static QTime addTime(const QTime & _orig, const QTime & _other, bool & _overflow);
+
+		static QTime subTime(const QTime & _orig, const QTime & _other, bool & _overflow);
 
 	};
 
--- src/akCore/aDateHelper.cpp
+++ src/akCore/aDateHelper.cpp
@@ -41,3 +41,59 @@ QTime ak::aDateHelper::addSeconds(const QTime & _orig, int _sec, bool & _overflo
 	return QTime(h, m, s, ms);
 
 }
+
+QTime ak::aDateHelper::addTime(const QTime & _orig, int _h, int _m, int _s, bool & _overflow) {
+	return addTime(_orig, _h, _m, _s, 0, _overflow);
+}
+
+QTime ak::aDateHelper::addTime(const QTime & _orig, int _h, int _m, int _s, int _ms, bool & _overflow) {
+	int h = _orig.hour();
+	int m = _orig.minute();
+	int s = _orig.second();
+	int ms = _orig.msec();
+
+	_overflow = false;
+
+	ms += _ms;
+	s += _s;
+	m += _m;
+	h += _h;
+
+	while (s < 0) {
+		s += 60;
+		m--;
+	}
+	while (s > 59) {
+		s -= 60;
+		m++;
+	}
+
+	while (m < 0) {
+		m += 60;
+		h--;
+	}
+	while (m > 59) {
+		m -= 60;
+		h++;
+	}
+
+	while (h < 0) {
+		_overflow = true;
+		h += 24;
+	}
+
+	while (h > 23) {
+		_overflow = true;
+		h -= 24;
+	}
+
+	return QTime(h, m, s, ms);
+}
+
+QTime ak::aDateHelper::addTime(const QTime & _orig, const QTime & _other, bool & _overflow) {
+	return addTime(_orig, _other.hour(), _other.minute(), _other.second(), _other.msec(), _overflow);
+}
+
+QTime ak::aDateHelper::subTime(const QTime & _orig, const QTime & _other, bool & _overflow) {
+	return addTime(_orig, _other.hour() * (-1), _other.minute() * (-1), _other.second() * (-1), _other.msec() * (-1), _overflow);
+}
