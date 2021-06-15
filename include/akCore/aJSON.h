cedb822d92beb8ee7a83e0c8d916460275f9bd34
Alexander Küster <alexkuester@mail.ru>
2021-06-15 14:40:56 +0200

    Added aJSON class

---
+++ include/akCore/aJSON.h
@@ -0,0 +1,285 @@
+/*
+ *	File:		aJSON.h
+ *	Package:	akCore
+ *
+ *  Created on: June 15, 2021
+ *	Author: Alexander Kuester
+ *  Copyright (c) 2020 Alexander Kuester
+ *	This file is part of the uiCore component.
+ *	This file is subject to the terms and conditions defined in
+ *	file 'LICENSE', which is part of this source code package.
+ */
+
+#pragma once
+
+#include <string>
+#include <map>
+#include <vector>
+
+#include <akCore/globalDataTypes.h>
+
+namespace ak {
+
+	class UICORE_API_EXPORT aJSON {
+	public:
+
+		enum jsonType {
+			Array,
+			Boolean,
+			Double,
+			Integer,
+			Null,
+			Object,
+			String
+		};
+
+		//! @brief Will create a empty item with the specified type
+		//! The type can not be changed
+		aJSON(jsonType _type);
+
+		//! @brief Will create an item with the type Boolean
+		//! @param _v The value to set
+		aJSON(bool _v);
+
+		//! @brief Will create an item with the type Double
+		//! @param _v The value to set
+		aJSON(double _v);
+
+		//! @brief Will create an item with the type Integer
+		//! @param _v The value to set
+		aJSON(int _v);
+
+		//! @brief Will create an item with the type String
+		//! @param _v The value to set
+		aJSON(char _v);
+
+		//! @brief Will create an item with the type String
+		//! @param _v The value to set
+		aJSON(const char * _v);
+
+		//! @brief Will create an item with the type String
+		//! @param _v The value to set
+		aJSON(const std::string & _v);
+
+		//! @brief Will create a copy of the other json item
+		//! @param _other The other item
+		aJSON(const aJSON & _other);
+
+		virtual ~aJSON();
+
+		// #############################################################
+
+		// Setter
+
+		//! @brief Will set the Boolean value
+		//! This function will have no effect if the item type is not Boolean
+		//! @param _v The value to set
+		void setBool(bool _v) { m_bool = _v; }
+
+		//! @brief Will set the Integer value
+		//! This function will have no effect if the item type is not Integer
+		//! @param _v The value to set
+		void setInt(int _v) { m_int = _v; }
+
+		//! @brief Will set the Double value
+		//! This function will have no effect if the item type is not Double
+		//! @param _v The value to set
+		void setDouble(double _v) { m_double = _v; }
+
+		//! @brief Will set the String value
+		//! This function will have no effect if the item type is not String
+		//! @param _v The value to set
+		void setString(char _v);
+
+		//! @brief Will set the String value
+		//! This function will have no effect if the item type is not String
+		//! @param _v The value to set
+		void setString(const char * _v) { m_string = _v; }
+
+		//! @brief Will set the String value
+		//! This function will have no effect if the item type is not String
+		//! @param _v The value to set
+		void setString(const std::string & _v) { m_string = _v; }
+
+		//! @brief Will reserve memory for the array
+		//! This function will have no effect if the item type is not Array
+		//! @param _size The size to reserve
+		void reserve(size_t _size);
+
+		//! @brief Will add the provided value to the end of the array
+		//! This function will have no effect if the item type is not Array
+		//! @param _v The value to add
+		void pushBack(bool _v);
+
+		//! @brief Will add the provided value to the end of the array
+		//! This function will have no effect if the item type is not Array
+		//! @param _v The value to add
+		void pushBack(int _v);
+
+		//! @brief Will add the provided value to the end of the array
+		//! This function will have no effect if the item type is not Array
+		//! @param _v The value to add
+		void pushBack(double _v);
+
+		//! @brief Will add the provided value to the end of the array
+		//! This function will have no effect if the item type is not Array
+		//! @param _v The value to add
+		void pushBack(char _v);
+
+		//! @brief Will add the provided value to the end of the array
+		//! This function will have no effect if the item type is not Array
+		//! @param _v The value to add
+		void pushBack(const char * _v);
+
+		//! @brief Will add the provided value to the end of the array
+		//! This function will have no effect if the item type is not Array
+		//! @param _v The value to add
+		void pushBack(const std::string & _v);
+
+		//! @brief Will add the provided value to the end of the array
+		//! This function will have no effect if the item type is not Array
+		//! @param _v The value to add
+		void pushBack(const aJSON & _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const char * _member, bool _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const char * _member, int _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const char * _member, double _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const char * _member, char _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const char * _member, const char * _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const char * _member, const std::string & _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const char * _member, const aJSON & _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const std::string & _member, bool _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const std::string & _member, int _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const std::string & _member, double _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const std::string & _member, char _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const std::string & _member, const char * _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const std::string & _member, const std::string & _v);
+
+		//! @brief Will add the provided member to the object
+		//! This function will have no effect if the item type is not Object
+		//! The order in which the members are added may differ from the order at export
+		//! @param _v The value to add
+		void addMember(const std::string & _member, const aJSON & _v);
+
+		// #############################################################
+
+		// Getter
+
+		//! @brief Will return the type of this json item
+		inline jsonType type(void) const { return m_type; }
+
+		//! @brief Will return a multiline JSON string representing this item
+		std::string toMultilineString(void) const;
+
+		//! @brief Will return the bool value
+		//! This function may be used only for bool type item
+		inline bool getBool(void) const { return m_bool; }
+
+		//! @brief Will return the double value
+		//! This function may be used only for double type item
+		inline double getDouble(void) const { return m_double; }
+
+		//! @brief Will return the integer value
+		//! This function may be used only for integer type item
+		inline int getInt(void) const { return m_int; }
+
+		//! @brief Will return null
+		//! This function may be used only for null type item
+		inline void * getNull(void) const { return nullptr; }
+
+		//! @brief Will return the string value
+		//! This function may be used only for string type item
+		inline std::string getString(void) { return m_string; }
+
+		//! @brief Will return true if the object has a member with the specified key
+		//! This function may be used only for object type item
+		bool hasMember(const std::string & _key) const;
+
+		//! @brief Will return the item with the specified member key
+		//! This function may be used only for object type item
+		aJSON getMember(const std::string & _key) const;
+
+		//! @brief Will return the item at the specified index
+		//! This function may be used only for array type item
+		aJSON getAt(size_t _index);
+
+	private:
+
+		std::string toMultilineString(const std::string & _prefix) const;
+
+		jsonType				m_type;
+
+		std::vector<aJSON>		m_array;
+		bool					m_bool;
+		double					m_double;
+		int						m_int;
+		std::map<std::string,
+			aJSON>				m_obj;
+		std::string				m_string;
+	};
+
+}
---
+++ src/akCore/aJSON.cpp
@@ -0,0 +1,251 @@
+/*
+ *	File:		aJSON.cpp
+ *	Package:	akCore
+ *
+ *  Created on: June 15, 2021
+ *	Author: Alexander Kuester
+ *  Copyright (c) 2020 Alexander Kuester
+ *	This file is part of the uiCore component.
+ *	This file is subject to the terms and conditions defined in
+ *	file 'LICENSE', which is part of this source code package.
+ */
+
+#include <akCore/aJSON.h>
+#include <cassert>
+
+ak::aJSON::aJSON(jsonType _type)
+	: m_type(_type), m_bool(false), m_double(0.0), m_int(0)
+{}
+
+ak::aJSON::aJSON(bool _v)
+	: m_type(Boolean), m_bool(_v), m_double(0.0), m_int(0)
+{}
+
+ak::aJSON::aJSON(double _v)
+	: m_type(Double), m_bool(false), m_double(_v), m_int(0)
+{}
+
+ak::aJSON::aJSON(int _v)
+	: m_type(Integer), m_bool(false), m_double(_v), m_int(_v)
+{}
+
+ak::aJSON::aJSON(char _v)
+	: m_type(String), m_bool(false), m_double(0.0), m_int(0)
+{
+	char v[2]{ _v, 0 };
+	m_string = v;
+}
+
+ak::aJSON::aJSON(const char * _v)
+	: m_type(String), m_bool(false), m_double(0.0), m_int(0), m_string(_v)
+{}
+
+ak::aJSON::aJSON(const std::string & _v)
+	: m_type(String), m_bool(false), m_double(0.0), m_int(0), m_string(_v)
+{}
+
+ak::aJSON::aJSON(const aJSON & _other)
+	: m_type(_other.type()), m_bool(false), m_double(0.0), m_int(0)
+{
+	switch (m_type)
+	{
+	case ak::aJSON::Array: m_array = _other.m_array; break;
+	case ak::aJSON::Boolean: m_bool = _other.m_bool; break;
+	case ak::aJSON::Double: m_double = _other.m_double; break;
+	case ak::aJSON::Integer: m_int = _other.m_int; break;
+	case ak::aJSON::Null: break;
+	case ak::aJSON::Object: m_obj = _other.m_obj; break;
+	case ak::aJSON::String: m_string = _other.m_string; break;
+	default:
+		assert(0);
+		break;
+	}
+}
+
+ak::aJSON::~aJSON() {
+
+}
+
+// ########################################################################################
+
+// Setter
+
+void ak::aJSON::setString(char _v) {
+	char v[2]{ _v, 0 };
+	m_string = v;
+}
+
+void ak::aJSON::reserve(size_t _size) {
+	if (m_type == Array) {
+		m_array.reserve(_size);
+	}
+}
+
+void ak::aJSON::pushBack(bool _v) {
+	if (m_type == Array) { m_array.push_back(_v); }
+}
+
+void ak::aJSON::pushBack(int _v) {
+	if (m_type == Array) { m_array.push_back(_v); }
+}
+
+void ak::aJSON::pushBack(double _v) {
+	if (m_type == Array) { m_array.push_back(_v); }
+}
+
+void ak::aJSON::pushBack(char _v) {
+	if (m_type == Array) { m_array.push_back(_v); }
+}
+
+void ak::aJSON::pushBack(const char * _v) {
+	if (m_type == Array) { m_array.push_back(_v); }
+}
+
+void ak::aJSON::pushBack(const std::string & _v) {
+	if (m_type == Array) { m_array.push_back(_v); }
+}
+
+void ak::aJSON::pushBack(const aJSON & _v) {
+	if (m_type == Array) { m_array.push_back(_v); }
+}
+
+void ak::aJSON::addMember(const char * _member, bool _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const char * _member, int _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const char * _member, double _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const char * _member, char _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const char * _member, const char * _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const char * _member, const std::string & _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const char * _member, const aJSON & _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const std::string & _member, bool _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const std::string & _member, int _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const std::string & _member, double _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const std::string & _member, char _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const std::string & _member, const char * _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+void ak::aJSON::addMember(const std::string & _member, const std::string & _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+
+void ak::aJSON::addMember(const std::string & _member, const aJSON & _v) {
+	if (m_type == Object) { m_obj.insert_or_assign(_member, _v); }
+}
+
+// ########################################################################################
+
+// Getter
+
+std::string ak::aJSON::toMultilineString(void) const {
+	return toMultilineString("");
+}
+
+bool ak::aJSON::hasMember(const std::string & _key) const {
+	if (m_type != Object) { return false; }
+	auto it = m_obj.find(_key);
+	return it != m_obj.end();
+}
+
+ak::aJSON ak::aJSON::getMember(const std::string & _key) const {
+	if (m_type != Object) { return aJSON(Null); }
+	auto it = m_obj.find(_key);
+	if (it != m_obj.end()) {
+		return it->second;
+	}
+	return aJSON(Null);
+}
+
+ak::aJSON ak::aJSON::getAt(size_t _index) {
+	if (m_type != Array || _index < 0 || _index >= m_array.size()) { return aJSON(Null); }
+	return m_array[_index];
+}
+
+// ########################################################################################
+
+// Private functions
+
+std::string ak::aJSON::toMultilineString(const std::string & _prefix) const {
+
+	switch (m_type)
+	{
+	case ak::aJSON::Array:
+	{
+		std::string ret{ "[" };
+		bool first{ true };
+
+		for (auto it : m_array) {
+			if (first) {
+				ret.append("\n"); first = false;
+			}
+			else {
+				ret.append(",\n");
+			}
+			ret.append(_prefix).append("\t");
+			ret.append(it.toMultilineString(_prefix + "\t"));
+		}
+		ret.append("\n").append(_prefix).append("]");
+		return ret;
+	}
+	case ak::aJSON::Boolean:
+		if (m_bool) { return "true"; }
+		else { return "false"; }
+	case ak::aJSON::Double: return std::to_string(m_int);
+	case ak::aJSON::Integer: return std::to_string(m_int);
+	case ak::aJSON::Object:
+	{
+		std::string ret{ "{" };
+		bool first{ true };
+
+		for (auto it : m_obj) {
+			if (first) {
+				ret.append("\n"); first = false;
+			}
+			else {
+				ret.append(",\n");
+			}
+			ret.append(_prefix).append("\t\"");
+			ret.append(it.first).append("\": ");
+			ret.append(it.second.toMultilineString(_prefix + "\t"));
+		}
+		ret.append("\n").append(_prefix).append("}");
+		return ret;
+	}
+	case ak::aJSON::String: return "\"" + m_string + "\"";
+	case ak::aJSON::Null:
+	default: return "null";
+	}
+}
